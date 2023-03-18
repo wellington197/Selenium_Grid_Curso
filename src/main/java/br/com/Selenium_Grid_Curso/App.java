4f55661ee18bb692e524563843b1bf59c065ef8a
wellington_197 <fcowellcosta@gmail.com>
2023-03-18 09:48:36 -0300

    Subindo testes JUnit do projeto de selenium para repositório

---
+++ src/main/java/br/com/Selenium_Grid_Curso/App.java
@@ -0,0 +1,8 @@
+package br.com.Selenium_Grid_Curso;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/br/com/Selenium_Grid_Curso/TesteGoogle.java
@@ -0,0 +1,25 @@
+package br.com.Selenium_Grid_Curso;
+
+import org.junit.*;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TesteGoogle {
+	
+	@Test
+	public void testeDeAcessoGoogle() {
+		
+		//Caso não tenha configurado nas variáveis de ambiente, terá de colocar o caminho
+		//System.setProperty("webdriver.gecko.driver", "/xampp/htdocs/CURSOS_WELLINGTON/CURSOS_UDEMY/Drivers/geckodriver.exe");
+		
+		WebDriver driver=new FirefoxDriver(); //instanciar o driver
+		
+		driver.get("http://www.google.com");
+		
+		System.out.println(driver.getTitle());
+		
+		
+	}
+
+}
