255f6fae96abdf9fe1b467c2aecfc63a9013147a
wellington_197 <fcowellcosta@gmail.com>
2023-03-27 07:51:10 -0300

    Criando interação e assert com campos de tipo texto

--- src/main/java/br/com/Selenium_Grid_Curso/TesteCampoTreinamento.java
+++ src/main/java/br/com/Selenium_Grid_Curso/TesteCampoTreinamento.java
@@ -46,37 +46,27 @@ public class TesteCampoTreinamento {
 
     @Test
     public void InteracaoComTextAreaField() {
+        /*-----PREPARAÇÃO-----*/
 
-        //Caso não tenha configurado nas variáveis de ambiente, terá de colocar o caminho
-        //System.setProperty("webdriver.gecko.driver", "/xampp/htdocs/CURSOS_WELLINGTON/CURSOS_UDEMY/Drivers/geckodriver.exe");
-
-
-        /*-----Prepara Cenário necessário para executar os testes-----*/
-
-        /*INSTANCIAR OS DRIVERS*/
+        //Instanciar o driver
         WebDriver driver=new FirefoxDriver(); //instanciar o driver do FIREFOX
-        //WebDriver driver=new ChromeDriver(); //instanciar o driver do CHROME
-        //WebDriver driver=new EdgeDriver(); //instanciar o driver do EDGE
-        //WebDriver driver=new InternetExplorerDriver(); //instanciar o driver do IE
-
-
         //Abre a tela de acordo com essa dimensão
         driver.manage().window().setSize(new Dimension(1250,765));
-
-        //Acesso ao site pelo protocolo
         // Usa um caminho dinâmico, onde busca software pela pasta raiz do projeto
         driver.get("file:///"+System.getProperty("user.dir")+"/src/main/resources/componentes.html");
-        //System.out.println(driver.getTitle());
 
+        /*--------EXECUÇÃO--------*/
 
         //Interagindo com campo de tipo texto
         driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("Teste de inserção de texto\n\nTeste de pular linha no campo");
-        //Pegar o texto do campo para validar
+
+
+        /*--------VERIFICAÇÃO--------*/
         Assert.assertEquals("Teste de inserção de texto\n\nTeste de pular linha no campo", driver.findElement(By.id("elementosForm:sugestoes")).getAttribute("value"));
-        /*--------Verificação--------*/
-        //Assert.assertEquals("Google",driver.getTitle());
 
         //fechar browser ap&oacute;s os testes
         //driver.quit();
     }
+
+
 }
