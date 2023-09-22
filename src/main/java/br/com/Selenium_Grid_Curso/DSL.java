7425489d134c1e5944c0ee5287d5e663308119d9
Wellington Costa <francisco.costa@itixti.com.br>
2023-09-21 21:49:02 -0300

    Criação do arquivo DSL para reuso do códigos

---
+++ src/main/java/br/com/Selenium_Grid_Curso/DSL.java
@@ -0,0 +1,46 @@
+package br.com.Selenium_Grid_Curso;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class DSL {
+
+    /**Instanciando uma variável global*/
+    private WebDriver driver;
+
+    public DSL(WebDriver driver) {
+        this.driver = driver;
+    }
+
+   public void escreve(String id_campo,String texto){
+       driver.findElement(By.id(id_campo)).sendKeys(texto);
+   }// fim do método escreve
+    public String obterValorCampo(String id_campo) {
+        return driver.findElement(By.id(id_campo)).getAttribute("value");
+    }
+
+    public void clicaRadio(String id){
+        driver.findElement(By.id(id)).click();
+    }
+    public boolean isRadioMarcado(String id){
+        return driver.findElement(By.id(id)).isSelected();
+    }
+    public void selecionarCombo(String id, String valor){
+        //Buscar os elementos
+        WebElement element = driver.findElement(By.id(id));
+        Select combo = new Select(element); //Seleciono um elemento
+
+        //combo.selectByIndex(5); //Seleciona o elemento 3
+        combo.selectByValue(valor); // Seleciona pelo valor
+        //combo.selectByVisibleText("Superior"); // (IDEAL) Valida se valor do campo é mesmo que usuário visualiza
+    }
+
+
+}
